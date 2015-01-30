0b2c432f00f6f79d9ca90eaeeffa58f405cca673
Sifan Ouyang <ouyangsifan@gmail.com>
2015-01-29 22:19:29 -0600

    hey i made it

--- js/script.js
+++ js/script.js
@@ -1,6 +1,39 @@
+var playerData;
+
 $(document).ready(function() {
-    console.log("Hello world.")
+    getData();
 });
+
+function getData() {
+	$.getJSON("js/juanbaseballcard.json", function(data, error) {
+		playerData = data;
+		drawStuff();
+	});
+}
+
+function drawStuff() {
+	
+	
+
+	var Name = playerData.name;
+	var Team = playerData.club;
+	var Stats = playerData.stats;
+
+
+
+	var lastSeasonIndex = Stats.length -1
+	var lastSeasonStats = playerData.stats[lastSeasonIndex];
+
+	var lastSeasonHomeRuns = lastSeasonStats.HR;
+	var lastSeasonYear = lastSeasonStats.year;
+	var lastSeasonTeam = lastSeasonStats.club;
+
+	$(".chart").append("<h1>"+Name+"</h1>");
+	$(".chart").append("<h3>"+Team+"</h3>");
+	$(".chart").append("<p>In "+lastSeasonYear+", "+Name+" hit "+lastSeasonHomeRuns+" home runs for the "+lastSeasonTeam);
+
+}
+
 
 
 
